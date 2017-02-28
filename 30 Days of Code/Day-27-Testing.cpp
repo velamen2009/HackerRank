c63a63cf5d0955f6c5b19182cd8d902674376bd5
velamen2009 <2565570621@qq.com>
2017-02-28 09:29:53 -0800

    30 Days of Code: Day 27

---
+++ 30 Days of Code/Day-27-Testing.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"5"<<endl; 
+    //YES
+    cout<<"6 3"<<endl; cout<<"-1 1 2 3 0 -2"<<endl;
+    //NO
+    cout<<"3 3"<<endl; cout<<"0 -1 1"<<endl;
+    //YES
+    cout<<"3 2"<<endl; cout<<"-1 0 1"<<endl;
+    //NO
+    cout<<"4 1"<<endl; cout<<"-1 0 1 -1"<<endl;
+    //YES
+    cout<<"4 3"<<endl; cout<<"-1 0 1 2"<<endl;   
+}
+
